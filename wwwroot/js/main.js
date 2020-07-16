2e9f897de199928cd778879f84b67392e624ed92
Marcus Kern <marcus@sorafamily.com>
2020-07-16 07:16:08 -0500

    updated header

--- wwwroot/js/main.js
+++ wwwroot/js/main.js
@@ -1,19 +1,7 @@
-System.register(["./sidebar/sidebar"], function (exports_1, context_1) {
-    "use strict";
-    var sidebar_1;
-    var __moduleName = context_1 && context_1.id;
-    return {
-        setters: [
-            function (sidebar_1_1) {
-                sidebar_1 = sidebar_1_1;
-            }
-        ],
-        execute: function () {
-            window.onload = function () {
-                var application = new sidebar_1.sidebar();
-                application.init();
-                console.log("application initialized");
-            };
-        }
-    };
-});
+import { sidebar } from "./sidebar/sidebar";
+window.onload = () => {
+    var application = new sidebar();
+    application.init();
+    console.log("application initialized");
+};
+//# sourceMappingURL=main.js.map
